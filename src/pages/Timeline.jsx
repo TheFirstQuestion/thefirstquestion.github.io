720c4ba7080c351ae190ff9df56899048d0adfdb
Steven G. Opferman <steven.g.opferman@gmail.com>
2024-05-17 19:10:08 -0400

    masonry layout now resizes columns to maximize size within a range

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -10,10 +10,50 @@ import { LinksContext } from "../contexts/LinksContext";
 export default function Timeline({ props }) {
 	const { links } = useContext(LinksContext);
 	const [data, setData] = useState([]);
-	// init one ref to store the future isotope object
 	const isotope = useRef();
-	// store the filter keyword in a state
+	const filterContainerRef = useRef();
 	const [filterKey, setFilterKey] = useState("*");
+	const [itemWidth, setItemWidth] = useState(0);
+	const minWidth = 400;
+	const maxWidth = 800;
+	const margin = getRemInPixels();
+	const [newMargin, setNewMargin] = useState(margin);
+
+	// Calculate the width of each column based on the container width
+	useEffect(() => {
+		const calculateItemWidth = () => {
+			if (filterContainerRef.current) {
+				const containerWidth = filterContainerRef.current.offsetWidth;
+				console.log("containerWidth", containerWidth);
+				// Calculate the largest number of columns that could fit in the container
+				const numColumns = Math.floor(containerWidth / (minWidth + margin * 2));
+				console.log("numColumns", numColumns);
+				// Calculate the maximum width of each item, if there are that many columns
+				const width = containerWidth / numColumns - numColumns * margin * 2;
+				console.log("width", width);
+				// Ensures the width is within the min and max bounds
+				const adjustedWidth = Math.max(minWidth, Math.min(maxWidth, width));
+				console.log("adjustedWidth", adjustedWidth);
+				// Recalculate margin to center the items perfectly
+				const newMargin =
+					(containerWidth - numColumns * adjustedWidth) / (numColumns * 2);
+				setNewMargin(newMargin);
+				setItemWidth(adjustedWidth);
+			}
+		};
+
+		const handleResize = () => {
+			calculateItemWidth();
+		};
+
+		window.addEventListener("resize", handleResize);
+		calculateItemWidth();
+
+		// Cleanup event listener
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, [margin]);
 
 	// Fetch the data from the JSON file
 	useEffect(() => {
@@ -39,6 +79,9 @@ export default function Timeline({ props }) {
 			isotope.current = new Isotope(".filter-container", {
 				itemSelector: ".filter-item",
 				layoutMode: "masonry",
+				masonry: {
+					fitWidth: true,
+				},
 			});
 		}
 	}, [data]);
@@ -49,7 +92,7 @@ export default function Timeline({ props }) {
 		filterKey === "*"
 			? isotope.current.arrange({ filter: `*` })
 			: isotope.current.arrange({ filter: `.${filterKey}` });
-	}, [filterKey]);
+	}, [filterKey, itemWidth]);
 
 	const handleFilterKeyChange = (key) => () => setFilterKey(key);
 
@@ -59,7 +102,10 @@ export default function Timeline({ props }) {
 
 	return (
 		<>
-			<Card className="border border-dark rounded text-center">
+			<Card
+				className="border border-dark rounded text-center"
+				ref={filterContainerRef}
+			>
 				<Card.Body>
 					<Card.Title>The Longest, Most In-Depth Resume Ever</Card.Title>
 					<Card.Text>
@@ -89,14 +135,21 @@ export default function Timeline({ props }) {
 
 			<div className="filter-container">
 				{data.map((item, index) => (
-					<ItemCard item={item} key={index} index={index} />
+					<ItemCard
+						item={item}
+						index={index}
+						style={{
+							width: itemWidth,
+							margin: `${newMargin}px`,
+						}}
+					/>
 				))}
 			</div>
 		</>
 	);
 }
 
-const ItemCard = ({ item, index }) => {
+const ItemCard = ({ item, index, style }) => {
 	const combinedItems = [];
 
 	// Add photos to combinedItems
@@ -130,9 +183,10 @@ const ItemCard = ({ item, index }) => {
 	// The cards
 	return (
 		<Card
+			// TODO: deal with spaces in tag names
 			className={classNames("filter-item", item.tags)}
 			key={index}
-			style={{ margin: "1rem", maxWidth: "500px" }}
+			style={style}
 		>
 			{/* TODO: equally space these vertically */}
 			<Card.Body>
@@ -275,3 +329,19 @@ const ItemVideo = ({ item }) => (
 		/>
 	</div>
 );
+
+/* ########################### Helper Functions ########################## */
+function tagToClass(tag) {
+	return tag.split(" ").join("_");
+}
+
+function classToTag(name) {
+	return name.split("_").join(" ");
+}
+
+function getRemInPixels() {
+	// Get the computed font size of the root element
+	const fontSize = getComputedStyle(document.documentElement).fontSize;
+	// Convert the font size from a string to a number (e.g., "16px" -> 16)
+	return parseFloat(fontSize);
+}
